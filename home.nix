b1fc7d5be2ab1045dbc1713f127e399e12c6136f
JohnFinn <dz4tune@gmail.com>
2024-04-14 01:56:02 +0200

    tmux config

--- home.nix
+++ home.nix
@@ -24,6 +24,8 @@
     dconf2nix
     git
     fish
+    tmux
+    nerdfonts
     alacritty
     google-chrome
 
@@ -83,6 +85,40 @@
     enable = true;
     vimAlias = true;
     extraConfig = builtins.readFile ./vimrc;
+    plugins = [ pkgs.vimPlugins.vim-visual-multi ];
+  };
+  programs.tmux = {
+    enable = true;
+    mouse = true;
+    extraConfig = ''
+      unbind r
+      bind r source-file ~/.config/tmux/tmux.conf
+      set-option -g status-position top
+      set -s escape-time 0
+    '';
+    plugins = with pkgs; [{
+      plugin = tmuxPlugins.catppuccin;
+      extraConfig = ''
+        set -g @catppuccin_window_left_separator ""
+        set -g @catppuccin_window_right_separator " "
+        set -g @catppuccin_window_middle_separator " █"
+        set -g @catppuccin_window_number_position "right"
+
+        set -g @catppuccin_window_default_fill "number"
+        set -g @catppuccin_window_default_text "#W"
+
+        set -g @catppuccin_window_current_fill "number"
+        set -g @catppuccin_window_current_text "#W"
+
+        set -g @catppuccin_status_modules_right "directory user host session"
+        set -g @catppuccin_status_left_separator  " "
+        set -g @catppuccin_status_right_separator ""
+        set -g @catppuccin_status_fill "icon"
+        set -g @catppuccin_status_connect_separator "no"
+
+        set -g @catppuccin_directory_text "#{pane_current_path}"
+      '';
+    }];
   };
 
   dconf.settings = with lib.hm.gvariant; {
