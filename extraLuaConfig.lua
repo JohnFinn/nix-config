c10ae2ebb1b7164136045f816f0d303faf84931e
JohnFinn <dz4tune@gmail.com>
2024-04-20 01:06:36 +0200

    formatting

--- extraLuaConfig.lua
+++ extraLuaConfig.lua
@@ -110,7 +110,7 @@ require("lspconfig").lua_ls.setup({
 require("lspconfig").nil_ls.setup({})
 
 vim.diagnostic.config({
-	virtual_text = {prefix = ''}, -- '' ''
+	virtual_text = { prefix = "" }, -- '' ''
 	signs = false,
 })
 
