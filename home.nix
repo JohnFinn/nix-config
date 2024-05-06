42ba79d43c78fd972f7ea314cb53993717fb722c
JohnFinn <dz4tune@gmail.com>
2024-05-06 22:19:29 +0200

    add anki

--- home.nix
+++ home.nix
@@ -50,6 +50,7 @@
     swappy
     python311Packages.ipython
     zathura
+    anki
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
