70441a478fc0a5cde39e1e6c49918cc5f3e35e9b
JohnFinn <dz4tune@gmail.com>
2024-08-19 23:28:18 +0200

    vim: change nix formatter to alejandra

--- nvim/extraLuaConfig.lua
+++ nvim/extraLuaConfig.lua
@@ -19,7 +19,7 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 
 require("conform").setup({
 	formatters_by_ft = {
-		nix = { "nixfmt" },
+		nix = { "alejandra" },
 		lua = { "stylua" },
 		cpp = { "clang_format" },
 		tex = { "latexindent" },
