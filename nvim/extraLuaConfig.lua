7c23ea478029884844301add762a89f59b32209a
JohnFinn <dz4tune@gmail.com>
2024-08-31 00:09:04 +0200

    vim: gitsigns keymappings

--- nvim/extraLuaConfig.lua
+++ nvim/extraLuaConfig.lua
@@ -5,7 +5,50 @@ vim.loader.enable()
 vim.cmd("colorscheme tokyonight-night")
 require("todo-comments").setup({ signs = false })
 require("Comment").setup()
-require("gitsigns").setup({})
+require("gitsigns").setup({
+	on_attach = function(bufnr)
+		local gitsigns = require("gitsigns")
+
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]h", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]h", bang = true })
+			else
+				gitsigns.next_hunk()
+			end
+		end)
+
+		map("n", "[h", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[h", bang = true })
+			else
+				gitsigns.prev_hunk()
+			end
+		end)
+
+		-- Actions
+		map("n", "<leader>hs", gitsigns.stage_hunk)
+		map("n", "<leader>hr", gitsigns.reset_hunk)
+		map("v", "<leader>hs", function()
+			gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		map("v", "<leader>hr", function()
+			gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+		end)
+		map("n", "<leader>hu", gitsigns.undo_stage_hunk)
+		map("n", "<leader>hp", gitsigns.preview_hunk_inline)
+		map("n", "<leader>td", gitsigns.toggle_deleted)
+
+		-- Text object
+		map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+	end,
+})
 require("mini.statusline").setup({})
 require("mini.surround").setup({})
 require("notify")
