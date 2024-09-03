83bb053c22918c565a70cedfb7d88b027e4adcf6
JohnFinn <dz4tune@gmail.com>
2024-09-03 12:19:17 +0200

    minor formatting

--- home.nix
+++ home.nix
@@ -142,7 +142,6 @@ in {
     # '')
   ];
 
-
   # NOTE: espanso won't start automatically. Use `espanso service start --unmanaged` to start it
   # FIXME: make it work on wayland
   services.espanso = {enable = true;};
