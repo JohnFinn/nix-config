24dacb9aed4cfcd2cda13c4f4929aec728056407
JohnFinn <dz4tune@gmail.com>
2024-09-04 17:28:34 +0200

    vim: tmux treesitter

--- home.nix
+++ home.nix
@@ -18,6 +18,7 @@
       html
       json
       vimdoc
+      tmux
     ]);
   vimPlugins = with pkgs.vimPlugins; [
     lazy-nvim
@@ -402,37 +403,45 @@ in {
       prefix = "C-a";
       baseIndex = 1;
       terminal = "screen-256color";
-      extraConfig = ''
-        unbind r
-        bind r source-file ~/.config/tmux/tmux.conf
-        set-option -g status-position top
-        set -s escape-time 0
-        set -ga terminal-overrides ',*256col*:Tc'
-      '';
+      extraConfig =
+        /*
+        tmux
+        */
+        ''
+          unbind r
+          bind r source-file ~/.config/tmux/tmux.conf
+          set-option -g status-position top
+          set -s escape-time 0
+          set -ga terminal-overrides ',*256col*:Tc'
+        '';
       tmuxinator.enable = true;
       plugins = with pkgs; [
         {
           plugin = tmuxPlugins.catppuccin;
-          extraConfig = ''
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator " "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
+          extraConfig =
+            /*
+            tmux
+            */
+            ''
+              set -g @catppuccin_window_left_separator ""
+              set -g @catppuccin_window_right_separator " "
+              set -g @catppuccin_window_middle_separator " █"
+              set -g @catppuccin_window_number_position "right"
 
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
+              set -g @catppuccin_window_default_fill "number"
+              set -g @catppuccin_window_default_text "#W"
 
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W"
+              set -g @catppuccin_window_current_fill "number"
+              set -g @catppuccin_window_current_text "#W"
 
-            set -g @catppuccin_status_modules_right "directory user host session"
-            set -g @catppuccin_status_left_separator  " "
-            set -g @catppuccin_status_right_separator ""
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
+              set -g @catppuccin_status_modules_right "directory user host session"
+              set -g @catppuccin_status_left_separator  " "
+              set -g @catppuccin_status_right_separator ""
+              set -g @catppuccin_status_fill "icon"
+              set -g @catppuccin_status_connect_separator "no"
 
-            set -g @catppuccin_directory_text "#{pane_current_path}"
-          '';
+              set -g @catppuccin_directory_text "#{pane_current_path}"
+            '';
         }
       ];
     };
