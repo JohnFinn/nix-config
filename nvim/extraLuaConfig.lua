ef59babfe52b243e118b5d4d97a86ca7f23df057
JohnFinn <dz4tune@gmail.com>
2024-09-05 23:13:36 +0200

    vim,starship: change git branch icon

--- nvim/extraLuaConfig.lua
+++ nvim/extraLuaConfig.lua
@@ -78,7 +78,34 @@ require("lazy").setup({
 			"echasnovski/mini.nvim",
 			dev = true,
 			config = function()
-				require("mini.statusline").setup({})
+				require("mini.statusline").setup({
+					content = {
+						-- Content for active window
+						active = function()
+							local mode, mode_hl = MiniStatusline.section_mode({ trunc_width = 120 })
+							local git = MiniStatusline.section_git({ trunc_width = 75, icon = "" })
+							local diagnostics = MiniStatusline.section_diagnostics({ trunc_width = 75 })
+							local filename = MiniStatusline.section_filename({ trunc_width = 140 })
+							local fileinfo = MiniStatusline.section_fileinfo({ trunc_width = 120 })
+							local location = MiniStatusline.section_location({ trunc_width = 75 })
+							local search = MiniStatusline.section_searchcount({ trunc_width = 75 })
+
+							return MiniStatusline.combine_groups({
+								{ hl = mode_hl, strings = { mode } },
+								{ hl = "MiniStatuslineDevinfo", strings = { git, diagnostics } },
+								"%<", -- Mark general truncate point
+								{ hl = "MiniStatuslineFilename", strings = { filename } },
+								"%=", -- End left alignment
+								{ hl = "MiniStatuslineFileinfo", strings = { fileinfo } },
+								{ hl = mode_hl, strings = { search, location } },
+							})
+						end,
+						-- Content for inactive window(s)
+						inactive = nil,
+					},
+				})
+				-- MiniStatusline.section_git({ args })
+
 				require("mini.surround").setup({})
 			end,
 		},
