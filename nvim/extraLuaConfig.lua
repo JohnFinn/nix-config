81969c7bc95f35cc714eda900ba3958e5dbef43d
JohnFinn <dz4tune@gmail.com>
2024-09-13 18:44:26 +0200

    vim: remove trailing slash

--- nvim/extraLuaConfig.lua
+++ nvim/extraLuaConfig.lua
@@ -69,7 +69,7 @@ require("lazy").setup({
 	spec = {
 		{
 			-- the colorscheme should be available when starting Neovim
-			"Mofiqul/vscode.nvim/",
+			"Mofiqul/vscode.nvim",
 			dev = true,
 			lazy = false, -- make sure we load this during startup if it is your main colorscheme
 			priority = 1000, -- make sure to load this before all the other start plugins
