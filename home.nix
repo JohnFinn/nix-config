10c7826f4592dedb5d72e569f9c73e1ca244ae54
JohnFinn <dz4tune@gmail.com>
2024-09-23 21:10:14 +0200

    gui: obsidian

--- home.nix
+++ home.nix
@@ -159,6 +159,7 @@ in {
       (python-pkgs: [python-pkgs.ipython python-pkgs.pandas python-pkgs.matplotlib python-pkgs.debugpy]))
     zathura
     anki
+    obsidian
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
