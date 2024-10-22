f6b758752c578366a323c590f2c67bd891946f52
JohnFinn <dz4tune@gmail.com>
2024-10-22 10:56:03 +0200

    wezterm: add comment

--- home.nix
+++ home.nix
@@ -139,6 +139,7 @@ in {
     alacritty
     kitty
     # using older version, cuz the new one has a bug https://github.com/wez/wezterm/issues/5990
+    # on Ubuntu I'm using wezterm-nightly package (version 20241015-083151-9ddca7bd) because it has better startuptime
     pkgs_old.wezterm
     watchexec
     jqp
