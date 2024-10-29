5648965a38bd65466c5c560a872b434eeb0b9f46
JohnFinn <dz4tune@gmail.com>
2024-10-29 17:55:39 +0100

    fetching source from github

--- home.nix
+++ home.nix
@@ -445,18 +445,25 @@ in {
 
                   inherit meta;
 
-                  src = fetchurl {inherit url sha256;};
+                  src = pkgs.fetchFromGitHub {
+                    owner = "JohnFinn";
+                    repo = "web_vim_remap";
+                    rev = "main";
+                    sha256 = "sha256-H4SIRSsXGZgArLRf0Ao12aN6ZUaIdGQ2Majo1xG26Ck=";
+                  };
 
                   preferLocalBuild = true;
                   allowSubstitutes = true;
 
                   passthru = {inherit addonId;};
 
+                  nativeBuildInputs = [pkgs.web-ext];
+
                   buildCommand = ''
+                    web-ext build --source-dir "$src"
                     dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
                     mkdir -p "$dst"
-                    # install -v -m644 "$src" "$dst/${addonId}.xpi"
-                    install -v -m644 "${/home/jouni/code/vim_editor_remap/web-ext-artifacts/hackce-0.1.zip}" "$dst/${addonId}.xpi"
+                    install -v -m644 "web-ext-artifacts/hackce-0.1.zip" "$dst/${addonId}.xpi"
                   '';
                 });
             in (
