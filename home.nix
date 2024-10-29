87471fa2b7bd55694925ac28289774b66659508d
JohnFinn <dz4tune@gmail.com>
2024-10-29 19:00:29 +0100

    refactoring changes

--- home.nix
+++ home.nix
@@ -431,9 +431,11 @@ in {
             (
               let
                 addonId = "hackce@nanigashi.stackoverflow";
+              in (pkgs.stdenv.mkDerivation {
+                name = "hackce-0.1";
+
                 meta = with lib; {
-                  homepage = "https://darkreader.org/";
-                  description = "Dark mode for every website. Take care of your eyes, use dark theme for night and daily browsing.";
+                  homepage = "https://github.com/JohnFinn/web_vim_remap";
                   license = licenses.mit;
                   mozPermissions = [
                     "webRequest"
@@ -442,33 +444,28 @@ in {
                   ];
                   platforms = platforms.all;
                 };
-              in (({stdenv ? pkgs.stdenv, ...}:
-                stdenv.mkDerivation {
-                  name = "hackce-0.1";
 
-                  inherit meta;
+                src = pkgs.fetchFromGitHub {
+                  owner = "JohnFinn";
+                  repo = "web_vim_remap";
+                  rev = "main";
+                  sha256 = "sha256-H4SIRSsXGZgArLRf0Ao12aN6ZUaIdGQ2Majo1xG26Ck=";
+                };
 
-                  src = pkgs.fetchFromGitHub {
-                    owner = "JohnFinn";
-                    repo = "web_vim_remap";
-                    rev = "main";
-                    sha256 = "sha256-H4SIRSsXGZgArLRf0Ao12aN6ZUaIdGQ2Majo1xG26Ck=";
-                  };
+                preferLocalBuild = true;
+                allowSubstitutes = true;
 
-                  preferLocalBuild = true;
-                  allowSubstitutes = true;
+                passthru = {inherit addonId;};
 
-                  passthru = {inherit addonId;};
+                nativeBuildInputs = [pkgs.web-ext];
 
-                  nativeBuildInputs = [pkgs.web-ext];
-
-                  buildCommand = ''
-                    web-ext build --source-dir "$src"
-                    dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-                    mkdir -p "$dst"
-                    install -v -m644 "web-ext-artifacts/hackce-0.1.zip" "$dst/${addonId}.xpi"
-                  '';
-                }) {})
+                buildCommand = ''
+                  web-ext build --source-dir "$src"
+                  dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
+                  mkdir -p "$dst"
+                  install -v -m644 "web-ext-artifacts/hackce-0.1.zip" "$dst/${addonId}.xpi"
+                '';
+              })
             )
           ];
         };
