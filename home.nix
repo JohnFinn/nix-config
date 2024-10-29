99b5ee5031b759ba9399ebb37bcd2313ddbca6f3
JohnFinn <dz4tune@gmail.com>
2024-10-29 19:36:20 +0100

    move derivation to separate file

--- home.nix
+++ home.nix
@@ -429,45 +429,7 @@ in {
             pkgs_firefox-addons.istilldontcareaboutcookies
             pkgs_firefox-addons.videospeed
             pkgs_firefox-addons.vimium # TODO: remap hjkl
-            (
-              let
-                addonId = "hackce@nanigashi.stackoverflow";
-              in (pkgs.stdenv.mkDerivation {
-                name = "hackce-0.1";
-
-                meta = with lib; {
-                  homepage = "https://github.com/JohnFinn/web_vim_remap";
-                  license = licenses.mit;
-                  mozPermissions = [
-                    "webRequest"
-                    "webRequestBlocking"
-                    "<all_urls>"
-                  ];
-                  platforms = platforms.all;
-                };
-
-                src = pkgs.fetchFromGitHub {
-                  owner = "JohnFinn";
-                  repo = "web_vim_remap";
-                  rev = "main";
-                  sha256 = "sha256-H4SIRSsXGZgArLRf0Ao12aN6ZUaIdGQ2Majo1xG26Ck=";
-                };
-
-                preferLocalBuild = true;
-                allowSubstitutes = true;
-
-                passthru = {inherit addonId;};
-
-                nativeBuildInputs = [pkgs.web-ext];
-
-                buildCommand = ''
-                  web-ext build --source-dir "$src"
-                  dst="$out/share/mozilla/extensions/{ec8030f7-c20a-464f-9b0e-13a3a9e97384}"
-                  mkdir -p "$dst"
-                  install -v -m644 "web-ext-artifacts/hackce-0.1.zip" "$dst/${addonId}.xpi"
-                '';
-              })
-            )
+            (import ./firefox_addon_web_vim_remap.nix {inherit pkgs lib;})
           ];
         };
         work = {
