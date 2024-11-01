c82494d17f8ba57ee1cabb69c306696110e8b213
JohnFinn <dz4tune@gmail.com>
2024-11-01 21:00:45 +0100

    move vim configuration to separate module

--- home.nix
+++ home.nix
@@ -5,86 +5,8 @@
   pkgs_firefox-addons,
   lib,
   ...
-}: let
-  treesitter = pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins:
-    with plugins; [
-      lua
-      cpp
-      c
-      nix
-      python
-      rust
-      bash
-      fish
-      latex
-      javascript
-      java
-      html
-      json
-      yaml
-      toml
-      vimdoc
-      tmux
-      markdown
-      todotxt
-      terraform
-      just
-      thrift
-      hcl
-      (helm.overrideAttrs
-        (oldAttrs: {
-          src = pkgs.fetchFromGitHub {
-            owner = "ngalaiko";
-            repo = "tree-sitter-go-template";
-            rev = "ca52fbfc98366c585b84f4cb3745df49f33cd140";
-            hash = "sha256-ZWpzqKD3ceBzlsRjehXZgu+NZMbWyyK+/R1Ymg7DVkM=";
-          };
-        }))
-    ]);
-  vimPlugins = with pkgs.vimPlugins; [
-    lazy-nvim
-    oil-nvim
-    telescope-nvim
-    telescope-live-grep-args-nvim
-    vim-rhubarb
-    copilot-vim
-    comment-nvim
-    conform-nvim
-    zen-mode-nvim
-    twilight-nvim
-    nvim-highlight-colors
-    gitsigns-nvim
-    vim-fugitive
-    flash-nvim
-    # for some reason old one has better startup time
-    pkgs_old.vimPlugins.auto-session
-    refactoring-nvim
-    # completion
-    nvim-cmp
-    cmp-nvim-lsp
-    luasnip
-    cmp_luasnip
-    cmp-path
-    neodev-nvim
-    nvim-lspconfig
-    # debugging
-    nvim-dap
-    nvim-dap-python
-    nvim-dap-ui
-    # TODO: change loading icon
-    fidget-nvim
-    nvim-notify
-    # -- theming
-    mini-nvim
-    noice-nvim
-    nvim-web-devicons
-    tokyonight-nvim
-    vscode-nvim
-    todo-comments-nvim
-    treesitter
-    nvim-treesitter-textobjects
-  ];
-in {
+}: {
+  imports = [./vim.nix];
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "jouni";
@@ -206,32 +128,6 @@ in {
     #   org.gradle.daemon.idletimeout=3600000
     # '';
     ".todo/config".source = ./dotfiles/todotxt-config;
-    ".config/nvim/lua/nix_paths.lua".text = let
-      load_treesitters_body = lib.strings.concatStrings (lib.strings.intersperse "\n"
-        (
-          lib.lists.forEach treesitter.dependencies
-          (
-            parser: let
-              lang = lib.strings.removePrefix "vimplugin-treesitter-grammar-" parser.name;
-            in
-              /*
-              lua
-              */
-              ''vim.treesitter.language.add("${lang}", {path = "${parser}/parser/${lang}.so"})''
-          )
-        ));
-    in
-      /*
-      lua
-      */
-      ''
-        return {
-        	lazypath = "${pkgs.vimPlugins.lazy-nvim}",
-        	load_treesitters = function ()
-        	${load_treesitters_body}
-        	end
-        }
-      '';
     ".config/wezterm/wezterm.lua".source = ./dotfiles/wezterm.lua;
     ".config/starship.toml".source = ./dotfiles/starship.toml;
     ".ideavimrc".source = ./dotfiles/nvim/ideavimrc;
@@ -525,32 +421,6 @@ in {
           name = ".ideavimrc";
           text = " ";
         }
-      ];
-    };
-    neovim = {
-      enable = true;
-      vimAlias = true;
-      extraLuaConfig = builtins.readFile ./dotfiles/nvim/extraLuaConfig.lua;
-
-      extraLuaPackages = luaPkgs: with luaPkgs; [nvim-nio pathlib-nvim];
-      plugins = vimPlugins;
-      extraPackages = with pkgs; [
-        wl-clipboard
-        stylua
-        black
-        alejandra
-        lua-language-server
-        nixd
-        pyright
-        nodePackages.bash-language-server
-        vscode-langservers-extracted
-        yaml-language-server
-        jdt-language-server
-        texlab
-        rust-analyzer
-        typescript-language-server
-        rustfmt
-        (python3.withPackages (python-pkgs: [python-pkgs.mdformat-gfm]))
       ];
     };
     tmux = {
---
+++ vim.nix
@@ -0,0 +1,138 @@
+{
+  pkgs,
+  pkgs_old,
+  lib,
+  ...
+}: let
+  treesitter = pkgs.vimPlugins.nvim-treesitter.withPlugins (plugins:
+    with plugins; [
+      lua
+      cpp
+      c
+      nix
+      python
+      rust
+      bash
+      fish
+      latex
+      javascript
+      java
+      html
+      json
+      yaml
+      toml
+      vimdoc
+      tmux
+      markdown
+      todotxt
+      terraform
+      just
+      thrift
+      hcl
+      (helm.overrideAttrs
+        (oldAttrs: {
+          src = pkgs.fetchFromGitHub {
+            owner = "ngalaiko";
+            repo = "tree-sitter-go-template";
+            rev = "ca52fbfc98366c585b84f4cb3745df49f33cd140";
+            hash = "sha256-ZWpzqKD3ceBzlsRjehXZgu+NZMbWyyK+/R1Ymg7DVkM=";
+          };
+        }))
+    ]);
+  vimPlugins = with pkgs.vimPlugins; [
+    lazy-nvim
+    oil-nvim
+    telescope-nvim
+    telescope-live-grep-args-nvim
+    vim-rhubarb
+    copilot-vim
+    comment-nvim
+    conform-nvim
+    zen-mode-nvim
+    twilight-nvim
+    nvim-highlight-colors
+    gitsigns-nvim
+    vim-fugitive
+    flash-nvim
+    # for some reason old one has better startup time
+    pkgs_old.vimPlugins.auto-session
+    refactoring-nvim
+    # completion
+    nvim-cmp
+    cmp-nvim-lsp
+    luasnip
+    cmp_luasnip
+    cmp-path
+    neodev-nvim
+    nvim-lspconfig
+    # debugging
+    nvim-dap
+    nvim-dap-python
+    nvim-dap-ui
+    # TODO: change loading icon
+    fidget-nvim
+    nvim-notify
+    # -- theming
+    mini-nvim
+    noice-nvim
+    nvim-web-devicons
+    tokyonight-nvim
+    vscode-nvim
+    todo-comments-nvim
+    treesitter
+    nvim-treesitter-textobjects
+  ];
+in {
+  programs.neovim = {
+    enable = true;
+    vimAlias = true;
+    extraLuaConfig = builtins.readFile ./dotfiles/nvim/extraLuaConfig.lua;
+
+    extraLuaPackages = luaPkgs: with luaPkgs; [nvim-nio pathlib-nvim];
+    plugins = vimPlugins;
+    extraPackages = with pkgs; [
+      wl-clipboard
+      stylua
+      black
+      alejandra
+      lua-language-server
+      nixd
+      pyright
+      nodePackages.bash-language-server
+      vscode-langservers-extracted
+      yaml-language-server
+      jdt-language-server
+      texlab
+      rust-analyzer
+      typescript-language-server
+      rustfmt
+      (python3.withPackages (python-pkgs: [python-pkgs.mdformat-gfm]))
+    ];
+  };
+  home.file.".config/nvim/lua/nix_paths.lua".text = let
+    load_treesitters_body = lib.strings.concatStrings (lib.strings.intersperse "\n"
+      (
+        lib.lists.forEach treesitter.dependencies
+        (
+          parser: let
+            lang = lib.strings.removePrefix "vimplugin-treesitter-grammar-" parser.name;
+          in
+            /*
+            lua
+            */
+            ''vim.treesitter.language.add("${lang}", {path = "${parser}/parser/${lang}.so"})''
+        )
+      ));
+  in
+    /*
+    lua
+    */
+    ''
+      return {
+      	lazypath = "${pkgs.vimPlugins.lazy-nvim}",
+      	load_treesitters = function ()
+      	${load_treesitters_body}
+      	end
+      }
+    '';
+}
