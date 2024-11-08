9e362671af842ec6c8c902231bc60b7f4219ba27
JohnFinn <dz4tune@gmail.com>
2024-11-08 22:58:25 +0100

    refactor: increase DRYness

--- home.nix
+++ home.nix
@@ -9,7 +9,7 @@
   # Home Manager needs a bit of information about you and the paths it should
   # manage.
   home.username = "jouni";
-  home.homeDirectory = "/home/jouni";
+  home.homeDirectory = "/home/${config.home.username}";
 
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
