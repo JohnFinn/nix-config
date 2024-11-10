639690e6fa9e56d4675cfa08ce9db51616a7957d
JohnFinn <dz4tune@gmail.com>
2024-11-10 13:51:08 +0100

    fish: add abbreviation

--- home.nix
+++ home.nix
@@ -194,6 +194,7 @@
         lg = "lazygit";
         gs = "git status";
         ga = "git add";
+        gco = "git checkout";
         ipy = "ipython";
         py = "python";
         ta = "todo.sh -t add";
