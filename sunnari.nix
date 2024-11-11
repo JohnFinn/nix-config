5841591b298160270703c3b98b6c180fa5e0af29
JohnFinn <dz4tune@gmail.com>
2024-11-11 12:19:10 +0100

    abbreviation with nixGL wrapper

--- sunnari.nix
+++ sunnari.nix
@@ -2,5 +2,8 @@
   imports = [./home.nix];
   home.username = "sunnari";
 
+  programs.fish.shellAbbrs = {
+    neovide = "nixGLIntel neovide";
+  };
   dconf.settings."org/gnome/desktop/input-sources".xkb-options = ["" "caps:swapescape"]; # TODO: replace with kanata
 }
