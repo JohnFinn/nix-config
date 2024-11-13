e83fd2c18210bf22c0cf1ce21afaec881ffbc6e9
JohnFinn <dz4tune@gmail.com>
2024-11-13 23:09:53 +0100

    fish: add abbrev

--- sunnari.nix
+++ sunnari.nix
@@ -5,6 +5,7 @@
   programs.fish.shellAbbrs = {
     neovide = "nixGLIntel neovide";
     anki = "nixGLIntel anki";
+    sudo = "sudo --preserve-env=PATH env";
   };
   dconf.settings."org/gnome/desktop/input-sources".xkb-options = ["" "caps:swapescape"]; # TODO: replace with kanata
 }
