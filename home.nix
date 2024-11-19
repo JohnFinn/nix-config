eb92e2f3a3196c6b50b98a9ebf33ad6605b2e481
JohnFinn <dz4tune@gmail.com>
2024-11-19 22:12:50 +0100

    easyeffects

--- home.nix
+++ home.nix
@@ -5,7 +5,20 @@
   lib,
   ...
 }: {
-  imports = [./vim.nix];
+  imports = [
+    ./vim.nix
+    ({pkgs, ...}: {
+      home.packages = [pkgs.easyeffects];
+      xdg.configFile = {
+        "easyeffects/output".source = pkgs.fetchFromGitHub {
+          owner = "Digitalone1";
+          repo = "EasyEffects-Presets";
+          rev = "32d0f416e7867ccffdab16c7fe396f2522d04b2e";
+          sha256 = "sha256-U9SSyHOOs8GsV6GBEqAqlBAuYONeh/4nkK8HurkEfWk=";
+        };
+      };
+    })
+  ];
 
   home.homeDirectory = "/home/${config.home.username}";
 
