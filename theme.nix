076859be6e51acb657404f6eeaf934aa72b99e7f
JohnFinn <dz4tune@gmail.com>
2024-11-24 20:08:29 +0100

    theming changes

--- theme.nix
+++ theme.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   home.packages = [pkgs.yaru-theme];
   dconf.settings = {
-    "org/gnome/desktop/interface".gtk-theme = "Yaru-dark";
+    "org/gnome/desktop/interface".gtk-theme = "Yaru-prussiangreen-dark";
     "org/gnome/desktop/interface".cursor-theme = "Yaru";
     "org/gnome/desktop/sound".theme-name = "Yaru";
   };
