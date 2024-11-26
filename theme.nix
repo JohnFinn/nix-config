1a5c773112a46cd0edb7e9c5f18e1ff28ebfbc77
JohnFinn <dz4tune@gmail.com>
2024-11-26 11:48:50 +0100

    add todo

--- theme.nix
+++ theme.nix
@@ -3,6 +3,6 @@
   dconf.settings = {
     "org/gnome/desktop/interface".gtk-theme = "Yaru-prussiangreen-dark";
     "org/gnome/desktop/interface".cursor-theme = "Yaru";
-    "org/gnome/desktop/sound".theme-name = "Yaru";
+    "org/gnome/desktop/sound".theme-name = "Yaru"; # TODO: change charging sound to adwaita
   };
 }
