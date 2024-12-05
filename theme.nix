b5dbcadca28080361c614d293a706f1438e6e162
JohnFinn <dz4tune@gmail.com>
2024-12-05 01:19:14 +0100

    gnome: configure sounds

--- theme.nix
+++ theme.nix
@@ -3,6 +3,23 @@
   dconf.settings = {
     "org/gnome/desktop/interface".gtk-theme = "Yaru-prussiangreen-dark";
     "org/gnome/desktop/interface".cursor-theme = "Yaru";
-    "org/gnome/desktop/sound".theme-name = "Yaru"; # TODO: change charging sound to adwaita
+    "org/gnome/desktop/sound".theme-name = "mysoundtheme";
+  };
+  home.file = {
+    ".local/share/sounds/mysoundtheme/index.theme".text = ''
+      [Sound Theme]
+      Name=mysoundteme
+      Directories=stereo
+
+      [stereo]
+      OutputProfile=stereo
+    '';
+    ".local/share/sounds/mysoundtheme/stereo/audio-volume-change.oga".source = pkgs.stdenvNoCC.mkDerivation {
+      name = "mysoundtheme";
+      installPhase = ''
+        cp ${pkgs.yaru-theme}/share/sounds/Yaru/stereo/audio-volume-change.oga $out
+      '';
+      phases = ["installPhase" "fixupPhase"];
+    };
   };
 }
