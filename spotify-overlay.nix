59a08890c500c30e4d5dcabfba73b0caafff0bbf
JohnFinn <dz4tune@gmail.com>
2024-12-11 00:25:20 +0100

    refactoring

--- spotify-overlay.nix
+++ spotify-overlay.nix
@@ -1,14 +1,11 @@
 # copy-paste from https://github.com/oskardotglobal/.dotfiles/blob/nix/default.nix
-final: prev: {
+final: prev: let
+  spotx = prev.fetchurl {
+    url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/9b8e3a6c443f5bde5803505105a569fac8510668/spotx.sh";
+    hash = "sha256-vry/wB5mcQUNeUUwwipzGwsZhxOJYJkc2w5z9vmcRdE=";
+  };
+in {
   spotify = prev.spotify.overrideAttrs (old: {
-    srcs = [
-      old.src
-      (prev.fetchurl {
-        url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/9b8e3a6c443f5bde5803505105a569fac8510668/spotx.sh";
-        hash = "sha256-vry/wB5mcQUNeUUwwipzGwsZhxOJYJkc2w5z9vmcRdE=";
-      })
-    ];
-
     nativeBuildInputs = old.nativeBuildInputs ++ (with prev; [util-linux perl unzip zip curl]);
 
     unpackPhase =
@@ -18,8 +15,8 @@ final: prev: {
       ]
       [
         ''
-          unsquashfs "$(echo $srcs | awk '{print $1}')" '/usr/share/spotify' '/usr/bin/spotify' '/meta/snap.yaml'
-          patchShebangs --build "$(echo $srcs | awk '{print $2}')"
+          unsquashfs "$src" '/usr/share/spotify' '/usr/bin/spotify' '/meta/snap.yaml'
+          patchShebangs --build ${spotx}
         ''
       ]
       old.unpackPhase;
@@ -29,7 +26,7 @@ final: prev: {
       ["runHook postInstall"]
       [
         ''
-          bash "$(echo $srcs | awk '{print $2}')" -f -P "$out/share/spotify"
+          bash ${spotx} -f -P "$out/share/spotify"
           runHook postInstall
         ''
       ]
