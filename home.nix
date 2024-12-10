cf99be9c62f0583be1531d4d06ac19f996536ab5
JohnFinn <dz4tune@gmail.com>
2024-12-10 20:38:09 +0100

    gnome: keymapping

--- home.nix
+++ home.nix
@@ -555,6 +555,7 @@
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
         "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
       ];
+      screensaver = ["<Super>l"];
     };
     "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
       binding = "<Super>Return";
