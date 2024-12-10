bc34d0a75dfd522c3a2db77ec37498ecbc13f981
JohnFinn <dz4tune@gmail.com>
2024-12-10 22:39:51 +0100

    removed xterm

--- configuration.nix
+++ configuration.nix
@@ -50,6 +50,7 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
+  services.xserver.excludePackages = [pkgs.xterm];
 
   environment.gnome.excludePackages = with pkgs; [
     baobab # disk usage analyzer
