d5b84fdd17a2cb3bb8d4b546f5327a35648d25bf
JohnFinn <dz4tune@gmail.com>
2024-12-14 15:50:42 +0100

    bench: testing with sleep in between

--- bench.sh
+++ bench.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
-FLAGS=(--max-runs 1)
-hyperfine "${FLAGS[@]}" --show-output "vim --cmd 'let g:exitAfterStart=1'"
-hyperfine "${FLAGS[@]}" 'fish -c true' '/bin/wezterm start true'
+FLAGS=(--max-runs 1 --prepare 'sleep .1s')
+
+bench() {
+  hyperfine "$@" --show-output "vim --cmd 'let g:exitAfterStart=1'"
+  hyperfine "$@" 'fish -c true' '/bin/wezterm start true'
+}
+
+bench "${FLAGS[@]}"
+echo with sleep | cowsay
+bench --min-runs 1000 --prepare 'sleep .1s'
+echo without sleep | cowsay
+bench --min-runs 1000
