468a5197bc455584d3cdda32935918e27a6b83db
JohnFinn <dz4tune@gmail.com>
2024-12-14 14:35:05 +0100

    refactor(bench): change string variable with an array

--- bench.sh
+++ bench.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-FLAGS="--max-runs 1"
-hyperfine $FLAGS --show-output "vim --cmd 'let g:exitAfterStart=1'"
-hyperfine $FLAGS 'fish -c true' '/bin/wezterm start true'
+FLAGS=(--max-runs 1)
+hyperfine "${FLAGS[@]}" --show-output "vim --cmd 'let g:exitAfterStart=1'"
+hyperfine "${FLAGS[@]}" 'fish -c true' '/bin/wezterm start true'
