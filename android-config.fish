6231d223c8a58858126c778e0ba80c8cc3d808f4
JohnFinn <dz4tune@gmail.com>
2024-12-26 00:42:28 +0100

    drafting android auto setup

---
+++ android-config.fish
@@ -0,0 +1,19 @@
+#!/usr/bin/env fish
+# adb install (bkt --ttl 1h -- curl https://f-droid.org/F-Droid.apk | psub --suffix .apk)
+
+function is_installed
+    adb shell cmd package list packages | rg "package:$argv\$"
+end
+
+function ensure_installed
+    # wait for user clicks
+    while not is_installed "$argv"
+        adb shell am start -a android.intent.action.VIEW -d "market://details?id=$argv"
+        sleep 1
+    end
+end
+
+ensure_installed com.whatsapp
+ensure_installed org.telegram.messenger
+echo foo
+
