ec01ebde77e15dd6def8b648bb52106f9fbd5a81
JohnFinn <dz4tune@gmail.com>
2024-12-29 01:45:18 +0100

    android: adb installing more packages

--- android-config.fish
+++ android-config.fish
@@ -1,13 +1,17 @@
 #!/usr/bin/env fish
 adb shell settings put system haptic_feedback_enabled 0
-adb install-multiple (bkt --ttl 1h -- curl https://f-droid.org/F-Droid.apk | psub --suffix .apk) (bkt --ttl 1h -- curl https://distractionfreeapps.com/build/dfinsta_1_4_1.apk | psub --suffix .apk)
-
-function is_installed
-    adb shell cmd package list packages | rg "package:$argv\$"
-end
 
 function fetch_from_fdroid_repo
     bkt --ttl 1h -- curl https://f-droid.org/repo/(bkt --ttl 1h -- curl https://f-droid.org/repo/index.xml | xq -r ".fdroid.application[] | select(.id == \"$argv\") .package[0].apkname")
+end
+
+adb install (bkt --ttl 1h -- curl https://f-droid.org/F-Droid.apk | psub --suffix .apk)
+adb install (bkt --ttl 1h -- curl https://distractionfreeapps.com/build/dfinsta_1_4_1.apk | psub --suffix .apk)
+adb install (fetch_from_fdroid_repo com.nutomic.syncthingandroid | psub --suffix .apk)
+adb install (fetch_from_fdroid_repo com.ichi2.anki | psub --suffix .apk)
+
+function is_installed
+    adb shell cmd package list packages | rg "package:$argv\$"
 end
 
 function screen_locked
@@ -32,6 +36,4 @@ end
 
 ensure_installed com.whatsapp
 ensure_installed org.telegram.messenger
-ensure_installed com.nutomic.syncthingandroid
-ensure_installed com.ichi2.anki
 ensure_installed mentz.com.vrr_cibo_app
