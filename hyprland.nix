cc0359421d3e9a25647dfb4ebb358dfcfaea8ec4
JohnFinn <dz4tune@gmail.com>
2025-01-13 00:09:22 +0100

    hyprpaper

--- hyprland.nix
+++ hyprland.nix
@@ -2,5 +2,13 @@
   home.packages = with pkgs; [
     pkgs.wofi
     pkgs.blueberry
+    pkgs.hyprpaper
   ];
+
+  xdg.configFile = {
+    "hypr/hyprpaper.conf".text = ''
+      preload = ${pkgs.gnome-backgrounds}/share/backgrounds/gnome/pixels-d.jpg
+      wallpaper = , ${pkgs.gnome-backgrounds}/share/backgrounds/gnome/pixels-d.jpg
+    '';
+  };
 }
