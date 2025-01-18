0b0ef7346d68efe30e729c80aaed9f930e47e36b
JohnFinn <dz4tune@gmail.com>
2025-01-18 23:18:12 +0100

    conky: less noise

--- dotfiles/conky/foo.sh
+++ dotfiles/conky/foo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 export PATH=$PATH:$HOME/.nix-profile/bin
-inotifywait ~/Sync/todo.txt/todo.txt
+inotifywait ~/Sync/todo.txt/todo.txt > /dev/null
 todo.sh -p list @sprint | sd '^\d+ |rec:\w+|due:\d{4}-\d{2}-\d{2}|t:\d{4}-\d{2}-\d{2}|\d{4}-\d{2}-\d{2}|@sprint' ''
