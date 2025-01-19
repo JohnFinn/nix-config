4f6bb435324ec5fad29a1dbdc28d8dae556d3054
JohnFinn <dz4tune@gmail.com>
2025-01-19 14:12:47 +0100

    conky: experiments with pidfile

--- dotfiles/conky/foo.sh
+++ dotfiles/conky/foo.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 export PATH=$PATH:$HOME/.nix-profile/bin
-inotifywait ~/Sync/todo.txt/todo.txt > /dev/null
+ppidfile=/tmp/.conky-todotxt
+invocation_id="$(ps -p $PPID -o pid,lstart --no-headers)"
+if [ -e $ppidfile ] && [ "$(cat $ppidfile)" == "$invocation_id" ]; then
+  inotifywait ~/Sync/todo.txt/todo.txt > /dev/null
+else
+  echo "$invocation_id" > $ppidfile
+fi
 todo.sh -p list @sprint | sd '^\d+ |rec:\w+|due:\d{4}-\d{2}-\d{2}|t:\d{4}-\d{2}-\d{2}|\d{4}-\d{2}-\d{2}|@sprint' ''
