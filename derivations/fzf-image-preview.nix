9edd302ddbcad868196b104a63b0ad4e13e21ac7
JohnFinn <dz4tune@gmail.com>
2025-05-25 14:27:29 +0200

    made preview work with ghostty

--- derivations/fzf-image-preview.nix
+++ derivations/fzf-image-preview.nix
@@ -1,17 +1,19 @@
 {
   imagemagick,
-  chafa,
+  kitty,
   bat,
+  gnused,
   writeShellApplication,
 }:
-# TODO: try using this to make ghostty work https://github.com/junegunn/fzf/blob/d18c0bf6948b4707684fe77631aff26a17cbc4fa/bin/fzf-preview.sh#L68
 writeShellApplication {
   name = "fzf-image-preview";
-  runtimeInputs = [imagemagick chafa bat];
+  runtimeInputs = [imagemagick kitty bat gnused];
   text = ''
     file="$1"
     if identify "$file" > /dev/null 2> /dev/null
-    then chafa -f iterm --view-size "$FZF_PREVIEW_COLUMNS"x"$FZF_PREVIEW_LINES" "$file"
+    # copy-pasted from https://github.com/junegunn/fzf/blob/d18c0bf6948b4707684fe77631aff26a17cbc4fa/bin/fzf-preview.sh#L68
+    then kitten icat --clear --transfer-mode=memory --unicode-placeholder --stdin=no --place="$FZF_PREVIEW_COLUMNS"x"$FZF_PREVIEW_LINES"'@0x0' "$file" | sed '$d' | sed $'$s/$/\e[m/'
+    # then chafa -f iterm --view-size "$FZF_PREVIEW_COLUMNS"x"$FZF_PREVIEW_LINES" "$file"
     else bat --color=always "$file"
     fi
   '';
