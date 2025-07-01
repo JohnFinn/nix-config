ec82e79800b5d5ef8e1799b42b593ea8145ca5ab
JohnFinn <dz4tune@gmail.com>
2025-07-01 22:53:14 +0200

    update spotx

--- spotify-overlay.nix
+++ spotify-overlay.nix
@@ -1,9 +1,12 @@
 # copy-paste from https://github.com/oskardotglobal/.dotfiles/blob/nix/overlays/spotx.nix
 final: prev: let
-  spotx = prev.fetchurl {
-    url = "https://raw.githubusercontent.com/SpotX-Official/SpotX-Bash/21481cea97bac720590c2aad8b1fc2c58c9ec8f9/spotx.sh";
-    hash = "sha256-1k1sEEnT1SE6RAWrfd1qFY1gFrUVNh7zUQJLu3DODlU=";
+  spotx = prev.fetchFromGitHub {
+    owner = "SpotX-Official";
+    repo = "SpotX-Bash";
+    rev = "2aaa003b4903d74d076dc85e6297a0caf93aea36";
+    sha256 = "sha256-IHXKJ3a5cGhpmkQ2crSq+rCpjocbDaSvKw5icP19lEw=";
   };
+  spotx_sh = "${spotx}/spotx.sh";
 in {
   spotify = prev.spotify.overrideAttrs (old: {
     nativeBuildInputs = old.nativeBuildInputs ++ (with prev; [util-linux perl unzip zip curl]);
@@ -13,7 +16,7 @@ in {
       ["runHook postUnpack"]
       [
         ''
-          patchShebangs --build ${spotx}
+          patchShebangs --build ${spotx_sh}
           runHook postUnpack
         ''
       ]
@@ -24,7 +27,7 @@ in {
       ["runHook postInstall"]
       [
         ''
-          bash ${spotx} -f -P "$out/share/spotify"
+          bash ${spotx_sh} -f -P "$out/share/spotify"
           runHook postInstall
         ''
       ]
